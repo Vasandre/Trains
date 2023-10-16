58a8ca0571c2c6330e212d3b1997323168a6ca4e
Vasandre <125184233+Vasandre@users.noreply.github.com>
2023-10-16 16:03:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -28,8 +28,6 @@ space = pymunk.Space()
 space.gravity = 0, -1000
 
 handler = space.add_default_collision_handler()
-
-touch_points = set()
 sprites = []
 
 scene = Scene()
@@ -46,9 +44,10 @@ players.append(player)
 
 key_vector = {"x": 0.0, "y": 0.0, "alpha": 0.0}
 
+
 # bot = Player(
 #     space=space,
-#     position=(600, 600),
+#     position=(600., 600.),
 #     angle=-player_angle,
 #     name="bot",
 #     color=(0, 255, 0)
@@ -72,9 +71,6 @@ def on_draw():
     window.clear()
     space.debug_draw(options)
     batch.draw()
-
-
-point_count = 0
 
 
 @window.event
@@ -118,9 +114,8 @@ def on_key_release(symbol, modifiers):
             key_vector["alpha"] = 0
 
 
+# object_count
 def update(dt):
-    global point_count
-
     if not player.train.auto:
         player.train.manual_update(*[key_vector[k] for k in ["x", "y", "alpha"]])
     else:
@@ -128,17 +123,21 @@ def update(dt):
         key_vector[1] = 0
         key_vector[2] = 0
 
+    sprites.clear()
+
     for item in players:
 
         data = item.update()
 
-        point_count += len(data['new_touch_points'])
         # отрисуем точки касания
-        for touch_point in list(data['new_touch_points']):
+        for touch_point in list(data['points']):
             sprites.append(shapes.Circle(touch_point[0], touch_point[1], 3, color=item.train.color, batch=batch))
 
-    if point_count % 100 == 0 and point_count > 0:
-        print(f"point count: {point_count}")
+        for line in data["lines"]:
+            sprites.append(shapes.Line(*line[0], *line[1], 3, color=line[2], batch=batch))
+
+        for circle in data["circles"]:
+            sprites.append(shapes.Arc(circle[0][0], circle[0][1], circle[1], color=circle[2], batch=batch))
 
     space.step(dt)
 
