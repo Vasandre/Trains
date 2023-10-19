f761b27072e659e18738efede4f8e5f2788d36a9
Vasandre <125184233+Vasandre@users.noreply.github.com>
2023-10-19 18:17:52 +0300

    Update ivan_train.py

--- game/ivan_train.py
+++ game/ivan_train.py
@@ -1,5 +1,6 @@
 from math import cos, sin, radians, sqrt
 from game.locator import Locator
+from random import randint
 
 
 class Train:
@@ -12,9 +13,14 @@ class Train:
                  ):
         """
         метод инициализации класса Train
+        :param x0: начальное положение по оси x
+        :param y0: начальное положение по оси y
+        :param alpha0: начальная ориентация (угол)
+        :param v_max: максимальная скорость
+        :param locator:
         """
 
-        # координаты мат точки
+        # координаты материальной точки
         self.x = x0
         self.y = y0
 
@@ -25,15 +31,30 @@ class Train:
 
         self.v = 0  # скорость точки
 
-        self.datas = []
         self.pointer = 0  # указатель на первую точку из диапазона
-        self.points = []
+        self.points = []  # список координат обнаруженных точек
+
+        self.data = {}  # словарь фигур
 
         # словарь фигур и точек
         self.figures = {
             "lines": [],
             "circles": [],
             "points": self.points
+        }
+
+        # словарь цветов
+        self.color = {
+            1: (0, 255, 255),
+            2: (127, 255, 212),
+            3: (118, 238, 198),
+            4: (102, 205, 170),
+            5: (69, 139, 116),
+            6: (223, 255, 0),
+            7: (127, 255, 0),
+            8: (118, 238, 0),
+            9: (102, 205, 0),
+            10: (69, 139, 0)
         }
 
         self.distance = None  # расстояние до препятствия
@@ -44,6 +65,9 @@ class Train:
     def update(self, x: float, y: float):
         """
         метод обновления информации, приходящей извне
+        :param x: координата по оси x
+        :param y: координата по оси y
+        :return:
         """
         if self.auto:
             self.x = x
@@ -78,6 +102,7 @@ class Train:
     def info(self) -> dict:
         """
         метод отображения текущего состояния
+        :return: словарь данных - координаты, скорость, угол и фигуры
         """
 
         return {
@@ -92,8 +117,8 @@ class Train:
     def is_circ(self, coordinates):
         """
         метод аппроксимации данных для получения координат и радиуса окружности
-        :param coordinates:
-        :return координаты центра окружности (x_c, y_c) и радиуса окружности r:
+        :param coordinates: список координат точек
+        :return: координаты центра окружности (x_c, y_c) и радиуса окружности r
         """
 
         equations = []  # список коээфициентов (k, b) для каждой прямой
@@ -144,8 +169,8 @@ class Train:
     def is_line(self, data):
         """
         метод аппроксимации данных в линию
-        :param data:
-        :return коэффициент наклона k, параметр b:
+        :param data: список координат точек
+        :return: коэффициент наклона k, параметр b
         """
         n = len(data)
         # суммы по координатам
@@ -190,7 +215,7 @@ class Train:
     def figure(self, mas):
         """
         метод определения фигуры
-        :param mas:
+        :param mas: список координат точек
         :return:
         """
         # угловой коэффициент и параметр прямой
@@ -210,34 +235,69 @@ class Train:
             k, b = self.is_line(mas)
             point1 = (mas[0][0], k * mas[0][0] + b)
             point2 = (mas[-1][0], k * mas[-1][0] + b)
-            self.figures["lines"].append((point1, point2))
+            return ["line", (point1, point2), mas]
         else:
-            self.figures["circles"].append((self.is_circ(mas)))
+
+            return ["circle", (self.is_circ(mas)), mas]
 
     def processing_auto(self):
-        """метод необходимых вычислений"""
-        self.alpha += radians(1)
+        """
+        метод необходимых вычислений
+        :return:
+        """
+        self.alpha += radians(2.67466418)
 
         # если встречается препятствие на пути
         if self.distance:
 
-            if len(self.points[self.pointer::]) > 10:
+            # если словарь пуст
+            if not self.data:
+                # параметров для вычисления должно быть как минимум 2
+                if len(self.points) > 2:
+                    self.data["1"] = self.figure(self.points.copy())
+                    self.points.clear()  # очистка списка
 
-                # сравниваем координаты
-                if (abs(self.points[-1][0] - self.points[-2][0]) >= 10) or \
-                        (abs(self.points[-1][1] - self.points[-2][1]) >= 10):
-                    self.figure(self.points[self.pointer:-1])
-                    self.pointer = len(self.points) - 1
+                    # очищаем словарь от окружностей и линий
+                    self.figures["lines"].clear()
+                    self.figures["circles"].clear()
 
-        # иначе нет препятствия
-        else:
+                    # заполняем словарь значений
+                    for key in self.data:
+                        if self.data[key][0] == "line":
+                            self.figures["lines"].append(self.data[key][1] + ((0, 25, 255),))
+                        else:
+                            self.figures["circles"].append(self.data[key][1] + ((0, 25, 255),))
 
-            # смотрим длину списка
-            if len(self.points[self.pointer::]) > 10:
-                self.figure(self.points[self.pointer::])
-                self.pointer = len(self.points) - 1  # смещение указателя
+            elif self.data and len(self.points) > 2:
+                self.data[str(len(self.data) + 1)] = self.figure(self.points.copy())
+                self.points.clear()  # очистка списка
+
             else:
-                if len(self.points) > 0:
-                    self.pointer = len(self.points) - 1
+
+                x_new, y_new = self.points[-1]
+                # проходим по всему словарю, а именно по точкам
+                for key in self.data:
+                    for elem in self.data[key][2]:
+                        x, y = elem
+
+                        dif_x = x - x_new
+                        dif_y = y - y_new
+
+                        if (0 <= abs(dif_x) < 40) and (0 <= abs(dif_y) < 40):
+                            self.data[key][2].append((x_new, y_new))
+                            self.data[key] = self.figure(self.data[key][2])
+                            self.points.pop()
+                            break
+
+                # очищаем словарь от окружностей и линий
+                self.figures["lines"].clear()
+                self.figures["circles"].clear()
+
+                # заполняем словарь значений
+                for key in self.data:
+                    if self.data[key][0] == "line":
+                        self.figures["lines"].append(self.data[key][1] + ((0, 25, 255),))
+                    else:
+                        self.figures["circles"].append(self.data[key][1] + ((0, 25, 255),))
 
         self.locator.make_query(self.x, self.y, self.alpha)
