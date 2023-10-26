5b3e59831937079c84b017b945f3e0b5d2dae1e6
Vasandre <125184233+Vasandre@users.noreply.github.com>
2023-10-26 23:21:50 +0300

    Update ivan_train.py

--- game/ivan_train.py
+++ game/ivan_train.py
@@ -1,6 +1,231 @@
-from math import cos, sin, radians, sqrt
+from math import cos, sin, radians, sqrt, tan, degrees, pi
 from game.locator import Locator
-from random import randint
+
+
+class Math:
+    def __init__(self):
+        self.coordinates = []  # список координат
+        self.equations = []  # список коээфициентов (k, b) для каждой прямой
+        self.intersections = []  # список координат точек пересечения
+
+    def add_list(self, coordinates):
+        self.coordinates = coordinates
+
+    def length(self):
+        return len(self.coordinates)
+
+    def middle_point(self):
+        """
+        метод вычисления координат середины отрезка
+        :return:
+        """
+
+        if self.equations:
+            self.equations.clear()
+
+        for number in range(len(self.coordinates) - 1):
+            x1, y1 = self.coordinates[number]
+            x2, y2 = self.coordinates[number + 1]
+
+            # координаты центра отрезка
+            x_mid = (x1 + x2) / 2
+            y_mid = (y1 + y2) / 2
+
+            # коэффициент наклона перпендикуляра к прямой
+            k = (y2 - y1) / (x2 + x1)
+
+            # коэффициенты уравнения прямой
+            k_p = - (1 / k)
+            b_p = y_mid - k_p * x_mid
+
+            self.equations.append((k_p, b_p))
+
+    def sums(self) -> tuple:
+        """
+        метод вычисления сумм по координатам x и y
+        :return: суммы по x и y
+        """
+
+        length = len(self.equations)
+
+        if self.intersections:
+            self.intersections.clear()
+
+        for i in range(length):
+            for j in range(i + 1, length):
+                k1, b1 = self.equations[i]
+                k2, b2 = self.equations[j]
+
+                # координаты пересечения
+                x_inter = (b2 - b1) / (k1 - k2)
+                y_inter = k1 * x_inter + b1
+
+                self.intersections.append((x_inter, y_inter))
+
+        # суммы по координатам x и y
+        x_sum = 0
+        y_sum = 0
+
+        for elem in self.intersections:
+            x_sum += elem[0]
+            y_sum += elem[1]
+
+        return x_sum, y_sum
+
+    def is_circle(self):
+        """
+         метод аппроксимации данных для получения координат центра и радиуса окружности
+        :return: координаты центра окружности (x_c, y_c) и радиус окружности r
+        """
+        self.middle_point()
+        sum_x, sum_y = self.sums()
+
+        len_inter = len(self.intersections)
+
+        # координаты центра окружности
+        x_c = sum_x / len_inter
+        y_c = sum_y / len_inter
+
+        # список вычисленных радиусов
+        radii = []
+
+        for point in self.coordinates:
+            x, y = point
+            radius = sqrt((x_c - x) ** 2 + (y_c - y) ** 2)
+            radii.append(radius)
+
+        # радиус окружности
+        r = sum(radii) / len(radii)
+
+        return (x_c, y_c), r
+
+    def approx_circ(self):
+        n = len(self.coordinates)
+        # сумма по координате x и y
+        sum_x = 0
+        sum_y = 0
+
+        # сумма квадратов по x и y
+        sum_x2 = 0
+        sum_y2 = 0
+
+        # сумма кубов по x и y
+        sum_x3 = 0
+        sum_y3 = 0
+
+        # сумма произведений
+        sum_xy = 0
+        sum_xy2 = 0
+        sum_x2y = 0
+
+        # проход по всем точкам
+        for point in self.coordinates:
+            x, y = point
+
+            sum_x += x
+            sum_y += y
+
+            sum_x2 += x ** 2
+            sum_y2 += y ** 2
+
+            sum_x3 += x ** 3
+            sum_y3 += y ** 3
+
+            sum_xy += x * y
+            sum_xy2 += x * (y ** 2)
+            sum_x2y += (x ** 2) * y
+
+        C = n * sum_x2 - sum_x * sum_x
+        D = n * sum_xy - sum_x * sum_y
+        E = n * sum_x3 + n * sum_xy2 - (sum_x2 + sum_y2) * sum_x
+        G = n * sum_y2 - sum_y * sum_y
+        H = n * sum_x2y + n * sum_y3 - (sum_x2 + sum_y2) * sum_y
+
+        a = (H * D - E * G) / (C * G - D * D)
+        b = (H * C - E * D) / (- (C * G - D * D))
+        c = - (a * sum_x + b * sum_y + sum_x2 + sum_y2) / n
+
+        x_c = a / (-2)
+        y_c = b / (-2)
+        r = sqrt(a ** 2 + b ** 2 - 4 * c) / 2
+
+        return (x_c, y_c), r
+
+    def is_line(self):
+        """
+        метод аппроксимации данных для получения уравнения прямой
+        :return:
+        """
+        # длина списка
+        len_coord = len(self.coordinates)
+
+        # суммы по координатам x и y
+        x_sum = 0
+        y_sum = 0
+
+        # сумма квадратов x
+        x2_sum = 0
+        # сумма произведений xy
+        xy_sum = 0
+
+        for point in self.coordinates:
+            x, y = point
+            x_sum += x
+            y_sum += y
+            x2_sum += x ** 2
+            xy_sum += x * y
+
+        k = (len_coord * xy_sum - x_sum * y_sum) / (len_coord * x2_sum - x_sum ** 2)
+        b = (y_sum - k * x_sum) / len_coord
+
+        return k, b
+
+    def sort_list(self) -> tuple:
+        x_min = min(self.coordinates, key=lambda c: c[0])[0]
+        x_max = max(self.coordinates, key=lambda c: c[0])[0]
+
+        diff_x = x_max - x_min
+
+        y_min = min(self.coordinates, key=lambda c: c[1])[1]
+        y_max = max(self.coordinates, key=lambda c: c[1])[1]
+
+        diff_y = y_max - y_min
+
+        return diff_x, diff_y
+
+
+class Objects:
+    def __init__(self, name):
+        self.name = name
+
+    def area(self):
+        pass
+
+
+class Line(Objects):
+    def __init__(self, name, k, b, info):
+        super().__init__(name)
+        self.k = k
+        self.b = b
+        self.info = info
+
+    def length(self):
+        x1, y1 = self.info[0]
+        x2, y2 = self.info[-1]
+
+        return sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+
+class Circle(Objects):
+    def __init__(self, name, x_c, y_c, r, info):
+        super().__init__(name)
+        self.x_c = x_c
+        self.y_c = y_c
+        self.r = r
+        self.info = info
+
+    def area(self):
+        return 2 * pi * self.r
 
 
 class Train:
@@ -30,11 +255,10 @@ class Train:
         self.v_max = v_max  # максимальная скорость точки
 
         self.v = 0  # скорость точки
+        print(self.v)
 
-        self.pointer = 0  # указатель на первую точку из диапазона
         self.points = []  # список координат обнаруженных точек
-
-        self.data = {}  # словарь фигур
+        self.math = Math()  # экземпляр класса математики
 
         # словарь фигур и точек
         self.figures = {
@@ -43,22 +267,18 @@ class Train:
             "points": self.points
         }
 
-        # словарь цветов
-        self.color = {
-            1: (0, 255, 255),
-            2: (127, 255, 212),
-            3: (118, 238, 198),
-            4: (102, 205, 170),
-            5: (69, 139, 116),
-            6: (223, 255, 0),
-            7: (127, 255, 0),
-            8: (118, 238, 0),
-            9: (102, 205, 0),
-            10: (69, 139, 0)
-        }
-
         self.distance = None  # расстояние до препятствия
-        self.rotation = 1  # угол поворота
+
+        self.modes = ["scan", "run", "rotate"]  # список режимов движения
+        self.mode = "scan" # режим движения
+
+        self.rotate = 1  # радиан поворота
+        self.scan_run = True  # True - сканирование, False - движение
+        self.run_rotate = True  # True - движение, False - поворот
+        self.count_run = 0
+
+        self.eps = 0.001  # пределе сканирования
+        self.count = 1
 
         self.auto = True  # управление в автоматическом режиме или нет
 
@@ -114,190 +334,74 @@ class Train:
         if self.auto:
             self.processing_auto()
 
-    def is_circ(self, coordinates):
-        """
-        метод аппроксимации данных для получения координат и радиуса окружности
-        :param coordinates: список координат точек
-        :return: координаты центра окружности (x_c, y_c) и радиуса окружности r
-        """
-
-        equations = []  # список коээфициентов (k, b) для каждой прямой
-        for num in range(len(coordinates) - 1):
-            # вычисление середины отрезка, соединяющего точки
-            x_mid = (coordinates[num][0] + coordinates[num + 1][0]) / 2
-            y_mid = (coordinates[num][1] + coordinates[num + 1][1]) / 2
-
-            # вычисление коэффициента наклона прямой
-            k = (coordinates[num + 1][1] - coordinates[num][1]) / (coordinates[num + 1][0] - coordinates[num][0])
-
-            # вычисление коээфицтентов (k, b) перпендикулярной прямой
-            k_p = - (1 / k)
-            b_p = y_mid - k_p * x_mid
-
-            equations.append((k_p, b_p))
-
-        intersections = []  # список координат точек пересечения
-
-        for i in range(len(equations)):
-            for j in range(i + 1, len(equations)):
-                x_inter = (equations[j][1] - equations[i][1]) / (equations[i][0] - equations[j][0])
-                y_inter = equations[i][0] * x_inter + equations[i][1]
-                intersections.append((x_inter, y_inter))
-
-        sum_x = 0  # сумма по координате x
-        sum_y = 0  # сумма по координате y
-
-        for elem in intersections:
-            sum_x += elem[0]
-            sum_y += elem[1]
-
-        # вычисление среднего значения точек пересечения
-        x_c = sum_x / len(intersections)
-        y_c = sum_y / len(intersections)
-
-        # список радиусов
-        radius = []
-
-        for el in coordinates:
-            radius.append(sqrt((el[0] - x_c) ** 2 + (el[1] - y_c) ** 2))
-
-        # усреднённый радиус окружности
-        r = sum(radius) / len(radius)
-
-        return (x_c, y_c), r
-
-    def is_line(self, data):
-        """
-        метод аппроксимации данных в линию
-        :param data: список координат точек
-        :return: коэффициент наклона k, параметр b
-        """
-        n = len(data)
-        # суммы по координатам
-        x_sum = 0
-        y_sum = 0
-        # сумма квадратов x
-        sum_x2 = 0
-        # сумма произведенеий x и y
-        sum_xy = 0
-
-        for dat in data:
-            x_sum += dat[0]
-            y_sum += dat[1]
-            sum_x2 += dat[0] ** 2
-            sum_xy += dat[0] * dat[1]
-
-        # средние значения по x и y
-        mean_x = x_sum / n
-        mean_y = y_sum / n
-
-        # разница между координатой и средним значением по координатам
-        dif_x = []
-        dif_y = []
-
-        for el in data:
-            dif_x.append(el[0] - mean_x)
-            dif_y.append(el[1] - mean_y)
-
-        # суммы произведений и квадратов
-        sum_dif_xy = 0
-        sum_dif_x2 = 0
-
-        for i in range(n):
-            sum_dif_xy += dif_x[i] * dif_y[i]
-            sum_dif_x2 += dif_x[i] ** 2
-
-        k = sum_dif_xy / sum_dif_x2  # коэффициент наклона k
-        b = mean_y - k * mean_x  # параметр b
-
-        return k, b
-
-    def figure(self, mas):
-        """
-        метод определения фигуры
-        :param mas: список координат точек
-        :return:
-        """
-        # угловой коэффициент и параметр прямой
-        coef, inc = self.is_line(mas)
-
-        temp = []  # временный список
-
-        for point in mas:
-            x, y = point
-            dist = abs(y - (coef * x + inc))
-            temp.append(dist)
-
-        sums = sum([x ** 2 for x in temp])
-        variance = sums / (len(mas) - 2)  # расчёт дисперсии
-
-        if -2 < variance < 2:
-            k, b = self.is_line(mas)
-            point1 = (mas[0][0], k * mas[0][0] + b)
-            point2 = (mas[-1][0], k * mas[-1][0] + b)
-            return ["line", (point1, point2), mas]
-        else:
-
-            return ["circle", (self.is_circ(mas)), mas]
-
     def processing_auto(self):
         """
         метод необходимых вычислений
         :return:
         """
-        self.alpha += radians(2.67466418)
 
-        # если встречается препятствие на пути
-        if self.distance:
+        print(self.v)
+        print("Дистанция", self.distance)
+        print("self.mode", self.mode)
+        print("self.scan_run", self.scan_run)
+        print("self.run_rotate", self.run_rotate)
 
-            # если словарь пуст
-            if not self.data:
-                # параметров для вычисления должно быть как минимум 2
-                if len(self.points) > 2:
-                    self.data["1"] = self.figure(self.points.copy())
-                    self.points.clear()  # очистка списка
+        # сканирование окружности
+        # если есть препятствие
+        if self.distance and self.scan_run:
+            self.mode = self.modes[0]
 
-                    # очищаем словарь от окружностей и линий
-                    self.figures["lines"].clear()
-                    self.figures["circles"].clear()
+        elif self.run_rotate is False:
+            self.mode = self.modes[1]
 
-                    # заполняем словарь значений
-                    for key in self.data:
-                        if self.data[key][0] == "line":
-                            self.figures["lines"].append(self.data[key][1] + ((0, 25, 255),))
-                        else:
-                            self.figures["circles"].append(self.data[key][1] + ((0, 25, 255),))
+        elif self.run_rotate:
+            self.mode = self.modes[2]
 
-            elif self.data and len(self.points) > 2:
-                self.data[str(len(self.data) + 1)] = self.figure(self.points.copy())
-                self.points.clear()  # очистка списка
+        match self.mode:
+            case "scan":
 
-            else:
+                # обновление списка
+                self.math.add_list(self.points)
 
-                x_new, y_new = self.points[-1]
-                # проходим по всему словарю, а именно по точкам
-                for key in self.data:
-                    for elem in self.data[key][2]:
-                        x, y = elem
+                if self.math.length() > 3:
+                    (x_c, y_c), r = self.math.approx_circ()
+                    self.figures["circles"] = [((x_c, y_c), r, (0, 25, 255))]
 
-                        dif_x = x - x_new
-                        dif_y = y - y_new
+                self.alpha += radians(self.rotate)
 
-                        if (0 <= abs(dif_x) < 40) and (0 <= abs(dif_y) < 40):
-                            self.data[key][2].append((x_new, y_new))
-                            self.data[key] = self.figure(self.data[key][2])
-                            self.points.pop()
-                            break
+                self.run_rotate = True
 
-                # очищаем словарь от окружностей и линий
-                self.figures["lines"].clear()
-                self.figures["circles"].clear()
+            case "run":
+                self.scan_run = False
+                if self.count_run < 36:
+                    print(self.v)
+                    self.x = self.x + self.v * cos(self.alpha)
+                    self.y = self.y + self.v * sin(self.alpha)
+                    self.count_run += 1
+                    self.run_rotate = False
+                else:
+                    print("STOP")
+                    self.count_run = 0
+                    self.run_rotate = True
 
-                # заполняем словарь значений
-                for key in self.data:
-                    if self.data[key][0] == "line":
-                        self.figures["lines"].append(self.data[key][1] + ((0, 25, 255),))
-                    else:
-                        self.figures["circles"].append(self.data[key][1] + ((0, 25, 255),))
+            case "rotate":
+                if self.count == 1:
+                    self.alpha += radians(15 - self.rotate)
+                    self.count += 1
+                    self.mode = self.modes[1]
+                    self.run_rotate = False
+
+                elif self.count == 2:
+                    self.alpha = self.alpha - radians(90)
+                    self.count += 1
+                    self.mode = self.modes[1]
+                    self.run_rotate = False
+
+                elif self.count == 3:
+                    self.alpha -= radians(105)
+                    self.count = 1
+                    self.mode = self.modes[0]
+                    self.run_rotate = True
+                    self.scan_run = True
 
         self.locator.make_query(self.x, self.y, self.alpha)
